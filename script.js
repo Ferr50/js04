9ec391ee59dc58b57691bf04db8c09c09f643c08
Fernando <ribeiro.fer50@gmail.com>
2021-11-23 23:25:31 -0300

    form

---
+++ script.js
@@ -0,0 +1,12 @@
+function submit() {
+
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var email = document.getElementById("email").value;
+    var msg = document.getElementById("msg").value;
+
+    alert("Nome: " + fname + "\nSobrenome: " + lname + "\nEmail: " + email + "\nMensagem: " + msg + "\n\nSUA MENSAGEM FOI ENVIADA COM SUCESSO");
+}
+
+
+
